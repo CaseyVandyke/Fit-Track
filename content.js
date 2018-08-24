dacfe24ec6cbdcef0a6f6d018106b4659038027d
Cryosith <caseyvandyke@ymail.com>
2018-08-24 16:59:49 -0600

    new html files

---
+++ content.js
@@ -0,0 +1,157 @@
+const profiles = [{
+        username: 'Bigmike',
+        status: 'away'
+
+    },
+    {
+        username: 'Musclehead',
+        status: 'active'
+
+    },
+    {
+        username: 'McGyver',
+        status: 'away'
+
+    },
+    {
+        username: 'Seth Rich',
+        status: 'active'
+
+    },
+    {
+        username: 'Maximus',
+        status: 'active'
+
+    },
+    {
+        username: 'Olly',
+        status: 'active'
+
+    },
+    {
+        username: 'slim Tim',
+        status: 'active'
+
+    },
+    {
+        username: 'Pete Johnson',
+        status: 'away'
+
+    },
+    {
+        username: 'ILoveChocolate',
+        status: 'active'
+
+    },
+
+
+];
+
+const routines = [{
+    workout: [{
+            cycle: 'biceps and triceps',
+            curls: '4 sets of 12 reps',
+            'preacher curls': '4 sets of 12 reps',
+            'isolated curls': '4 sets of 12 reps',
+            'cable curls': '4 sets of 12 reps'
+        },
+        {
+            cycle: 'biceps and triceps',
+            curls: '4 sets of 12 reps',
+            'preacher curls': '4 sets of 12 reps',
+            'isolated curls': '4 sets of 12 reps',
+            'cable curls': '4 sets of 12 reps'
+        },
+        {
+            cycle: 'biceps and triceps',
+            curls: '4 sets of 12 reps',
+            'preacher curls': '4 sets of 12 reps',
+            'isolated curls': '4 sets of 12 reps',
+            'cable curls': '4 sets of 12 reps'
+        },
+        {
+            cycle: 'biceps and triceps',
+            curls: '4 sets of 12 reps',
+            'preacher curls': '4 sets of 12 reps',
+            'isolated curls': '4 sets of 12 reps',
+            'cable curls': '4 sets of 12 reps'
+        },
+        {
+            cycle: 'biceps and triceps',
+            curls: '4 sets of 12 reps',
+            'preacher curls': '4 sets of 12 reps',
+            'isolated curls': '4 sets of 12 reps',
+            'cable curls': '4 sets of 12 reps'
+        },
+        
+
+    ]
+
+
+}];
+
+
+const dietPlans = [{
+    meal: [{
+            name: 'Almond Chicken',
+            recipe: 'chicken breast 1 cup almonds 1 tsp season salt',
+            directions: `Marinade chicken pieces in garlic,ginger,soy and cornstarch for 15 to 20 minute.
+            Mix sauce ingredients together in small bowl, set aside.
+             Heat wok with 1 tablespoons oil and stir fry almonds until lightly browned; set aside.
+            Heat wok with 1 tablespoons oil; add water chestnuts and snow peas and stir fry for 30 seconds, remove and set aside.
+            Heat wok with 2 tbsp oil and stir fry chicken pieces until they turn white and firm ( about 2 minutes).
+            Add cooked vegetables and almonds to cooked chicken in wok; mix and add sauce mixture.
+            Stir together until sauce thickens slightly.
+            Serve with rice or noodles.`
+        },
+        {
+            name: 'Almond Chicken',
+            recipe: 'chicken breast 1 cup almonds 1 tsp season salt',
+            directions: `Marinade chicken pieces in garlic,ginger,soy and cornstarch for 15 to 20 minute.
+            Mix sauce ingredients together in small bowl, set aside.
+             Heat wok with 1 tablespoons oil and stir fry almonds until lightly browned; set aside.
+            Heat wok with 1 tablespoons oil; add water chestnuts and snow peas and stir fry for 30 seconds, remove and set aside.
+            Heat wok with 2 tbsp oil and stir fry chicken pieces until they turn white and firm ( about 2 minutes).
+            Add cooked vegetables and almonds to cooked chicken in wok; mix and add sauce mixture.
+            Stir together until sauce thickens slightly.
+            Serve with rice or noodles.`
+        },
+        {
+            name: 'Almond Chicken',
+            recipe: 'chicken breast 1 cup almonds 1 tsp season salt',
+            directions: `Marinade chicken pieces in garlic,ginger,soy and cornstarch for 15 to 20 minute.
+            Mix sauce ingredients together in small bowl, set aside.
+             Heat wok with 1 tablespoons oil and stir fry almonds until lightly browned; set aside.
+            Heat wok with 1 tablespoons oil; add water chestnuts and snow peas and stir fry for 30 seconds, remove and set aside.
+            Heat wok with 2 tbsp oil and stir fry chicken pieces until they turn white and firm ( about 2 minutes).
+            Add cooked vegetables and almonds to cooked chicken in wok; mix and add sauce mixture.
+            Stir together until sauce thickens slightly.
+            Serve with rice or noodles.`
+        },
+        {
+            name: 'Almond Chicken',
+            recipe: 'chicken breast 1 cup almonds 1 tsp season salt',
+            directions: `Marinade chicken pieces in garlic,ginger,soy and cornstarch for 15 to 20 minute.
+            Mix sauce ingredients together in small bowl, set aside.
+             Heat wok with 1 tablespoons oil and stir fry almonds until lightly browned; set aside.
+            Heat wok with 1 tablespoons oil; add water chestnuts and snow peas and stir fry for 30 seconds, remove and set aside.
+            Heat wok with 2 tbsp oil and stir fry chicken pieces until they turn white and firm ( about 2 minutes).
+            Add cooked vegetables and almonds to cooked chicken in wok; mix and add sauce mixture.
+            Stir together until sauce thickens slightly.
+            Serve with rice or noodles.`
+        },
+        {
+            name: 'Almond Chicken',
+            recipe: 'chicken breast 1 cup almonds 1 tsp season salt',
+            directions: `Marinade chicken pieces in garlic,ginger,soy and cornstarch for 15 to 20 minute.
+            Mix sauce ingredients together in small bowl, set aside.
+             Heat wok with 1 tablespoons oil and stir fry almonds until lightly browned; set aside.
+            Heat wok with 1 tablespoons oil; add water chestnuts and snow peas and stir fry for 30 seconds, remove and set aside.
+            Heat wok with 2 tbsp oil and stir fry chicken pieces until they turn white and firm ( about 2 minutes).
+            Add cooked vegetables and almonds to cooked chicken in wok; mix and add sauce mixture.
+            Stir together until sauce thickens slightly.
+            Serve with rice or noodles.`
+        }
+
+    ]
+}];
