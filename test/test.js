1186c73996e94e74c93576ccafdbd76759cd94c3
Cryosith <caseyvandyke@ymail.com>
2018-08-25 13:02:37 -0600

    added js file

--- test/test.js
+++ test/test.js
@@ -1,36 +1,21 @@
-const isEqual = require('../isEqual');
 
-const expect = require('chai').expect;
+"use strict";
 
-describe('isEqual', function() {
-    it('should give right answers for equal and unequal inputs', function() {
-        // we're going to the `isEqual` function on a range of inputs
-        // that the function should return true for.
-        const equalInputs = [
-            [1, 1],
-            [true, true],
-            ['foo', 'foo']
-        ];
-        // loop through inputs and check that each set returns true
-        equalInputs.forEach(function(input) {
-            const answer = isEqual(input[0], input[1]);
-            // use `chai.expect`'s keywords to set expectations for
-            // `isEqual`'s behavior
-            expect(answer).to.be.true;
-        });
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../server.js");
 
-        // range of inputs that the function should return false for
-        const unequalInputs = [
-            ['1', 1],
-            [1, 2],
-            [1, true],
-            [0, false]
-        ];
-        // loop through inputs and check that each set returns false
-        unequalInputs.forEach(function(input) {
-            const answer = isEqual(input[0], input[1]);
-            expect(answer).to.be.false;
-        });
-    });
-  }
-);
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe("index page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get("/")
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
