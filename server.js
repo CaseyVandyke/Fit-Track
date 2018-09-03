62280e52d0fb8873424399967d9a1529c81ea37a
Cryosith <caseyvandyke@ymail.com>
2018-09-03 16:29:05 -0600

    test

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 
 const express = require('express');
-const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
 const routineRouter = require('./routineRouter');
 
-const jsonParser = bodyParser.json();
 const app = express();
 
 app.use(morgan('common'));
-
 app.use('/routines', routineRouter);
-
-
 app.use(express.static('public'));
+
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
   });
