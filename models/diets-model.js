a4c16340104feb202f37f1d799a7d745bd24a3e0
Cryosith <caseyvandyke@ymail.com>
2018-09-15 13:41:55 -0600

    test

--- models/diets-model.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
--- routers/dietRouter.js
+++
@@ -1,3 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Diet = require('../models/diets-model');
